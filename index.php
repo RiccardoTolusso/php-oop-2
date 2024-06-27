429066ccbc3abe9c94d1e9762c1f75bffb94c603
RiccardoTolusso <riccardotolusso215@gmail.com>
2024-06-27 08:05:35 +0200

    [UPD] cards layout and items class

--- index.php
+++ index.php
@@ -16,10 +16,10 @@ class Product
     protected string $animal;
     protected string $description;
     protected string $category;
-
+    protected int $stock;
 
     # constructor
-    public function __construct(string $name, string $image, float $price, int | null $discount, string $animal, string $description, string $category)
+    public function __construct(string $name, int $stock, string $image, float $price, int | null $discount, string $animal, string $description, string $category)
     {
         $this->name = strtoupper($name);
         $this->image = $image;
@@ -30,13 +30,14 @@ class Product
         $this->description = $description;
         $this->category = strtoupper($category);
         $this->id = $this->generateId();
+        $this->stock = $stock;
     }
 
     # GETTERS
     protected function generateId()
     {
         $num = self::$product_number++;
-        $id = "$this->animal-$this->category-$num";
+        $id = "$num-$this->animal-$this->category";
         return $id;
     }
 
@@ -45,7 +46,7 @@ class Product
         return $this->name;
     }
 
-    public function getPrice(): float
+    public function getPrice(): float | null
     {
         return $this->price;
     }
@@ -55,14 +56,24 @@ class Product
         return $this->fullPrice;
     }
 
-    public function getDiscount(): int | null
+    public function getDiscount(): string | null
     {
-        return $this->discount;
+        if (!is_null($this->discount)) {
+            return $this->discount . "%";
+        } else {
+            return null;
+        }
     }
 
     public function getAnimal(): string
     {
-        return $this->animal;
+        if ($this->animal === "CAT") {
+            return "Gatto";
+        } elseif ($this->animal === "DOG") {
+            return "Cane";
+        } else {
+            return "Animali";
+        }
     }
 
     public function getDescription(): string
@@ -85,6 +96,11 @@ class Product
         return $this->id;
     }
 
+    public function getStock(): int
+    {
+        return $this->stock;
+    }
+
     #SETTERS
     public function setPrice(float $price, float | null $discount)
     {
@@ -104,9 +120,9 @@ class Food extends Product
     private array $ingredients;
 
     #constructor polimorph of Product constructor
-    public function __construct(string $name, string $image,  float $price, int | null $discount, string $animal, string $description, string $category, float $weight, array $ingredients)
+    public function __construct(string $name, int $stock, string $image,  float $price, int | null $discount, string $animal, string $description, string $category, float $weight, array $ingredients)
     {
-        parent::__construct($name, $image, $price, $discount, $animal, $description, $category);
+        parent::__construct($name, $stock,  $image, $price, $discount, $animal, $description, $category);
 
         $this->weight = $weight;
         $this->ingredients = $ingredients;
@@ -114,8 +130,42 @@ class Food extends Product
 
     public function getAttributes(): string
     {
-        $ingredients = implode(", ", $this->ingredients);
-        return "Peso: $this->weight Kg <br>Ingredienti: $ingredients";
+        $res = "Peso: $this->weight Kg";
+        if (count($this->ingredients) > 0) {
+            $ingredients = implode(", ", $this->ingredients);
+            $res .= "<br>Ingredienti: $ingredients";
+        }
+
+        return $res;
+    }
+}
+
+
+#class Object EXTENDED FROM product
+class Item extends Product
+{
+    #private properties
+    private string $dimensions;
+    private array $materials;
+
+    #constructor polimorph of Product constructor
+    public function __construct(string $name, int $stock, string $image,  float $price, int | null $discount, string $animal, string $description, string $category, string $dimensions, array $materials)
+    {
+        parent::__construct($name, $stock,  $image, $price, $discount, $animal, $description, $category);
+
+        $this->dimensions = $dimensions;
+        $this->materials = $materials;
+    }
+
+    public function getAttributes(): string
+    {
+        $res = $this->dimensions;
+        if (count($this->materials) > 0) {
+            $materials = implode(", ", $this->materials);
+            $res .= "<br>Materiali: $materials";
+        }
+
+        return $res;
     }
 }
 
@@ -125,7 +175,13 @@ $text = file_get_contents(__DIR__ . '/db/productsDB.json');
 $products_categories = json_decode($text, true);
 
 foreach ($products_categories['food'] as $prodcut) {
-    $tempFood = new Food($prodcut['name'], $prodcut['image'], $prodcut['price'], $prodcut['discount'], $prodcut['animal'], $prodcut['description'], "food", $prodcut['weight'], $prodcut['ingredients']);
+    $tempFood = new Food($prodcut['name'], $prodcut['stock'], $prodcut['image'], $prodcut['price'], $prodcut['discount'], $prodcut['animal'], $prodcut['description'], "food", $prodcut['weight'], $prodcut['ingredients']);
+
+    $products["food"][] = $tempFood;
+}
+
+foreach ($products_categories['doghouse'] as $prodcut) {
+    $tempFood = new Item($prodcut['name'], $prodcut['stock'], $prodcut['image'], $prodcut['price'], $prodcut['discount'], $prodcut['animal'], $prodcut['description'], "food", $prodcut['dimensions'], $prodcut['materials']);
 
     $products["food"][] = $tempFood;
 }
@@ -146,14 +202,52 @@ $result = [...$products['food']];
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
-<body>
+<body class="bg-dark">
     <div class="container py-5">
-        <div class="row row-cols-6 gap-5">
+        <div class="row row-cols-4 gap-5">
             <?php foreach ($result as $key => $prod) : ?>
-                <div class="card">
+                <div class="card text-center text-bg-light pt-2">
+
+                    <!-- BADGES -->
+                    <div class="position-absolute end-0 top-0 d-flex">
+                        <div class="badge bg-success fs-6 m-0"><?php echo $prod->getDiscount() ?></div>
+                        <div class="badge bg-secondary fs-6"><?php echo $prod->getAnimal() ?></div>
+                    </div>
+
+                    <!-- IMMAGINE PRODOTTO -->
                     <img class="card-img-top" src="<?php echo $prod->getImage() ?>" alt="product image">
+
+                    <!-- CARD BODY -->
                     <div class="card-body">
-                        <h5 class="card-title"><?php echo $prod->getName() ?></h5>
+
+                        <!-- NOME PRODOTTO -->
+                        <h5 class="card-title">
+                            <?php echo $prod->getName() ?>
+                        </h5>
+
+                        <!-- PREZZO PRODOTTO -->
+                        <h6 class="card-subtitle mb-3">
+                            <?php if (is_null($prod->getDiscount())) : ?>
+                                <?php echo $prod->getFullPrice(); ?>
+                            <?php else : ?>
+                                <span><?php echo $prod->getPrice() ?></span>
+                                <del class="text-danger"><?php echo $prod->getFullPrice() ?></del>
+                            <?php endif ?>
+                        </h6>
+
+                        <!-- DESCRIZIONE -->
+                        <p class="card-text"><?php echo $prod->getDescription() ?></p>
+                        <!-- ATTRIBUTI -->
+                        <p class="card-text"><?php echo $prod->getAttributes() ?></p>
+                    </div>
+
+                    <!-- CARD FOOTER -->
+                    <div class="card-footer text-body-secondary">
+                        <?php if ($prod->getStock() === 0) : ?>
+                            <button href="" class="btn btn-secondary" disabled>Scorte Esaurite</button>
+                        <?php else : ?>
+                            <button href="" class="btn btn-primary">Aggiungi al carrello</button>
+                        <?php endif ?>
                     </div>
                 </div>
             <?php endforeach ?>
